c7ba37dec0ed1ee5dae046f3df7e405279ae0d78
Jim Rhotert <rhotert@stud.uni-hannover.de>
2022-01-09 16:18:40 +0100

    logic is hard

--- src/agent.jl
+++ src/agent.jl
@@ -4,7 +4,7 @@ end
 
 function move(board::Boardstate, you_index::Int, simulation_time::Float64)
     # time stuff
-    if length(board.snakes > 2)
+    if length(board.snakes) > 2
         minimax_action = iterative_deepening_maxn(board, simulation_time, board.height, board.width, you_index)
     else
         minimax_action = iterative_deepening(board, simulation_time, board.height, board.width, you_index)
