e528ab71cf2f5eb4fa4ff70570ee0703d7d1b53e
Jim Rhotert <rhotert@stud.uni-hannover.de>
2022-01-09 15:57:01 +0100

    maxn if needed

--- src/agent.jl
+++ src/agent.jl
@@ -4,7 +4,11 @@ end
 
 function move(board::Boardstate, you_index::Int, simulation_time::Float64)
     # time stuff
-    minimax_action = iterative_deepening(board, simulation_time, board.height, board.width, you_index)
+    if length(board.snakes > 2)
+        minimax_action = iterative_deepening_maxn(board, simulation_time, board.height, board.width, you_index)
+    else
+        minimax_action = iterative_deepening(board, simulation_time, board.height, board.width, you_index)
+    end
     return minimax_action
 end
 
