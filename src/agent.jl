05a0956899e608cb44a017cdfcd9b6091dd075f6
Jim Rhotert <rhotert@stud.uni-hannover.de>
2022-01-12 15:43:08 +0100

    less sim time for maxn

--- src/agent.jl
+++ src/agent.jl
@@ -5,6 +5,7 @@ end
 function move(board::Boardstate, you_index::Int, simulation_time::Float64)
     # time stuff
     if length(board.snakes) > 2
+        simulation_time -= 0.05
         minimax_action = iterative_deepening_maxn(board, simulation_time, board.height, board.width, you_index)
     else
         minimax_action = iterative_deepening(board, simulation_time, board.height, board.width, you_index)
